d07d9c73a8b2bd9e5a41793e6f867ae0e6835970
Manuel Sigaran <manuel.psigaran@gmail.com>
2026-08-28 00:23:32 +0100

    Configuracion inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Component/Body/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // Carga Home por defecto
+  { path: '**', redirectTo: '' } // Si escriben una URL que no existe, los regresa a Home
+];
