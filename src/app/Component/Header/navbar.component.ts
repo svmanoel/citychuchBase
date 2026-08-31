5f9437ee73156ff33dc3e52778ff0ab9585a1d17
Manuel Sigaran <manuel.psigaran@gmail.com>
2026-08-31 18:18:41 +0100

    se agergan estykis y se crea una version preliminar del navbar

--- src/app/Component/Header/navbar.component.ts
+++ src/app/Component/Header/navbar.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
- styles: []
+  styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+    menuOpen = false;
+
+  activeDropdown: string | null = null;
+
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+
+    // Cerramos los dropdowns al cerrar el menú
+    if (!this.menuOpen) {
+      this.activeDropdown = null;
+    }
+  }
+
+
+  toggleDropdown(menu: string): void {
+
+    if (this.activeDropdown === menu) {
+      this.activeDropdown = null;
+    } else {
+      this.activeDropdown = menu;
+    }
+
+  }
+
+
+  closeMenu(): void {
+    this.menuOpen = false;
+    this.activeDropdown = null;
+  }
+
 
 }
