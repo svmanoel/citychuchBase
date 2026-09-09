8dd70de43247f8881552afce2f891a4663616b11
Manuel Sigaran <manuel.psigaran@gmail.com>
2026-09-09 12:00:30 +0100

    cambios en el footer y en el  header

--- src/app/Component/Footer/footer.component.ts
+++ src/app/Component/Footer/footer.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
-  styles: []
+  styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+
+  currentYear = new Date().getFullYear();
 
 }
