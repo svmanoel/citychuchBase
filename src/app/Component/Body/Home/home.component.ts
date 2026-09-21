0374c230d4319aba756ffdbd3277d808f9d64850
Manuel Sigaran <manuel.psigaran@gmail.com>
2026-09-22 00:55:20 +0100

    se agergan imagenes

--- src/app/Component/Body/Home/home.component.ts
+++ src/app/Component/Body/Home/home.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+        CommonModule,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './home.component.html',
  styleUrl: './home.component.css'
 })
